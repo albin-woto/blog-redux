720c9a04fdd4ba3e377ed7dc1bf0741f8a255abd
albin-woto <albin.woto@gmail.com>
2020-07-16 15:12:58 -0300

    store created for reducers, userReducer created in reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducers from './reducers';
 
 import './index.css';
 
 import App from './components/App/App';
 
+const store = createStore(
+  reducers, // All reducers
+  {} // Initial state
+);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={ store }>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
