69bbcf431b6d9004992acfdc07be8e5ce67efbf5
albin-woto <albin.woto@gmail.com>
2020-07-21 18:21:03 -0300

    try/catch in userActions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,11 +2,15 @@ import axios from 'axios';
 import { GET_ALL } from '../types/userTypes';
 
 export const getAllUsers = () => async (dispatch) => {
-  const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/users'
-  );
-  dispatch({
-    type: GET_ALL,
-    payload: response.data,
-  });
+  try {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    dispatch({
+      type: GET_ALL,
+      payload: response.data,
+    });
+  } catch(error) {
+    console.error(error.message);
+  }
 };
