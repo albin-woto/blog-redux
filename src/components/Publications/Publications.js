5d4607f68f93f0c6389facede47b861e88e35380
albin-woto <albin.woto@gmail.com>
2020-07-29 16:21:47 -0300

    Add userReducer to Publications component

--- src/components/Publications/Publications.js
+++ src/components/Publications/Publications.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as userActions from '../../actions/userActions';
 
-export default class Publications extends Component {
+class Publications extends Component {
+  componentDidMount() {
+    if (!this.props.users.length) {
+      this.props.getAllUsers();
+    }
+  }
+
   render() {
     return (
       <div>
-        { this.props.match.params.key }
+        <h1>Publications of {this.props.users.name}</h1>
+        {this.props.match.params.key}
       </div>
-    )
+    );
   }
 }
+
+const mapStateToProps = (reducers) => {
+  return reducers.userReducer;
+};
+
+export default connect(mapStateToProps, userActions)(Publications);
