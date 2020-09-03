e1f496e7947aea440bfb889345918179127b79fc
albin-woto <albin.woto@gmail.com>
2020-09-03 14:06:38 -0300

    Included the tranding component and fixed some styles, not responsive yet

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,18 +3,19 @@ import React from 'react';
 import './Home.css';
 import Background from '../../images/home-background.jpg';
 
+import Trending from '../../components/Trending/Trending';
+
 const Home = () => {
   return (
-    <main className="home-container">
+    <div className="home-container">
       <img
         className="home-background"
         src={Background}
         alt="Coffe, tablet, pencil and books on a desk"
       />
-      <h1 className="title">
-        Welcome to <strong>The blog</strong>
-      </h1>
-    </main>
+      <h1 className="title">The blog</h1>
+      <Trending />
+    </div>
   );
 };
 
