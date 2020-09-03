4ebe0888ff904122099e961360e7761d008e9b8c
albin-woto <albin.woto@gmail.com>
2020-09-03 14:05:04 -0300

    Created a card with a simple publication info

---
+++ src/components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Card.css';
+
+const Card = () => {
+  return (
+    <article className="card">
+      <div className="card-info">
+        <h4 className="card-title">Title</h4>
+        <h6 className="card-author">from <strong>Name</strong></h6>
+      </div>
+      <p className="card-body">
+        loremipsumkaj ajsdnajsn alsjfnalsfnl lajknflansf asjdfa ajshfkajsh ajsfh
+        akjsfh ajsdfh akjsfh jahsfkjashdf ajsfhkas asfasjf ha osfhoas oaifhs oai
+        hso hoaisfh aosifh oaishfoiash f aiohsf oiahsf oaisfh oaihsf oaisfh
+        oiahs f
+      </p>
+    </article>
+  );
+};
+
+export default Card;
