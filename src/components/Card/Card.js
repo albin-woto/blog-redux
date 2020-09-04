9f8c4366f0a476d7fd11fb7b10e41b122f872acd
albin-woto <albin.woto@gmail.com>
2020-09-03 22:23:29 -0300

    Gravatar with styles in trending cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './Card.css';
 
+import Gravatar from '../Gravatar/Gravatar';
+
 const Card = (props) => {
   const {users} = props;
   return (
     <article className="card">
       <div className="card-info">
+        <Gravatar 
+          className="card-avatar"
+          email={users.email}
+          alt="Avatar"
+        />
         <h4 className="card-title">Title</h4>
+      </div>
         <h6 className="card-author">
           from <strong>{users.name}</strong>
           <br />
           in <strong>{users.website}</strong>
         </h6>
-      </div>
       <p className="card-body">
         loremipsumkaj ajsdnajsn alsjfnalsfnl lajknflansf asjdfa ajshfkajsh ajsfh
         akjsfh ajsdfh akjsfh jahsfkjashdf ajsfhkas asfasjf ha osfhoas oaifhs oai
