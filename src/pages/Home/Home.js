836167ad8ba561ff506522d282ee4121fcb83c64
albin-woto <albin.woto@gmail.com>
2020-09-08 00:12:32 -0300

    responsive tablet for Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './Home.css';
+import Image from '../../images/image.png';
 
 import Trending from '../../components/Trending/Trending';
 
@@ -8,7 +9,14 @@ const Home = () => {
   return (
     <div className="home-container">
       <h1 className="title">The blog</h1>
-      <Trending />
+      <section className="home-content">
+        <Trending />
+        <img
+          className="home-image"
+          src={Image}
+          alt="Drawing of a girl reading"
+        />
+      </section>
     </div>
   );
 };
