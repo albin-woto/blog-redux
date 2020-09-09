2660e57b24119987a9128281be5e341715860d63
albin-woto <albin.woto@gmail.com>
2020-09-09 19:39:56 -0300

    Updates

---
+++ precache-manifest.783b56046ae3b5fa2b9b2b5805ca2eb7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4829d940f7b63ca9c283de4d4a8568a9",
+    "url": "/blog-redux/index.html"
+  },
+  {
+    "revision": "f1f01ed4ce010670efde",
+    "url": "/blog-redux/static/css/4.798617e1.chunk.css"
+  },
+  {
+    "revision": "1a512b932a84c4fcfe89",
+    "url": "/blog-redux/static/css/5.bde044ff.chunk.css"
+  },
+  {
+    "revision": "4571e2c6d4d97a576ed7",
+    "url": "/blog-redux/static/css/6.27859b26.chunk.css"
+  },
+  {
+    "revision": "6d917a84da189b5ef3c7",
+    "url": "/blog-redux/static/css/7.6f597378.chunk.css"
+  },
+  {
+    "revision": "9a4f9701e701f5ac6d24",
+    "url": "/blog-redux/static/css/8.234970b8.chunk.css"
+  },
+  {
+    "revision": "212a0b54b7ac31795462",
+    "url": "/blog-redux/static/css/main.35e7ba7a.chunk.css"
+  },
+  {
+    "revision": "f20716be1a79611b521f",
+    "url": "/blog-redux/static/js/0.37c1161b.chunk.js"
+  },
+  {
+    "revision": "3df54bba2137ec524f3fb39f2c61461a",
+    "url": "/blog-redux/static/js/0.37c1161b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53654171354b3b2fb30",
+    "url": "/blog-redux/static/js/3.9ab2aa2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog-redux/static/js/3.9ab2aa2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f01ed4ce010670efde",
+    "url": "/blog-redux/static/js/4.81e8b9ab.chunk.js"
+  },
+  {
+    "revision": "1a512b932a84c4fcfe89",
+    "url": "/blog-redux/static/js/5.de3e0c96.chunk.js"
+  },
+  {
+    "revision": "4571e2c6d4d97a576ed7",
+    "url": "/blog-redux/static/js/6.0b58e03f.chunk.js"
+  },
+  {
+    "revision": "6d917a84da189b5ef3c7",
+    "url": "/blog-redux/static/js/7.2b1f773b.chunk.js"
+  },
+  {
+    "revision": "9a4f9701e701f5ac6d24",
+    "url": "/blog-redux/static/js/8.7d302145.chunk.js"
+  },
+  {
+    "revision": "212a0b54b7ac31795462",
+    "url": "/blog-redux/static/js/main.ad4f8aa3.chunk.js"
+  },
+  {
+    "revision": "4df398dd4cc2b5e90b41",
+    "url": "/blog-redux/static/js/runtime-main.ffff792a.js"
+  },
+  {
+    "revision": "ddc250127189a3b4ee4802e6c2493757",
+    "url": "/blog-redux/static/media/github.ddc25012.svg"
+  },
+  {
+    "revision": "3c8afea5ee97ad99924808a2ab5fec10",
+    "url": "/blog-redux/static/media/linkedin.3c8afea5.svg"
+  }
+]);
